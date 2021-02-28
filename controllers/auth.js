0c925cc8f5080d52201f35f94deb1523723e2fad
Guillermo González Jiménez <ggjwork@gmail.com>
2021-02-28 10:18:32 +0100

    Google SignIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 const login = async(req = request, res = response) => {
@@ -55,6 +56,52 @@ const login = async(req = request, res = response) => {
 }
 
 
+const googleSingIn = async(req = request, res = response) => {
+    const { id_token } = req.body
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token)
+
+        let usuario = await Usuario.findOne({ correo })
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario(data)
+            await usuario.save()
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Usuario bloqueado'
+            })
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id)
+
+
+        res.json({
+            msg: 'Todo ok google signin',
+            usuario,
+            token
+        })
+    }
+    catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no válido'
+        })
+    }
+}
+
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
-const { login } = require('../controllers/auth')
+const { login, googleSingIn } = require('../controllers/auth')
 
 const { validarCampos } = require('../middlewares/validar-campos')
 
@@ -16,6 +16,16 @@ router.post(
         validarCampos
     ],
     login
+)
+
+
+router.post(
+    '/google',
+    [
+        check('id_token', 'El id_token es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    googleSingIn
 )
 
 
